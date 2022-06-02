9ac462d6d7053e731209842c6e5c09506623c4c9
Torrin Leonard <82110564+torrinworx@users.noreply.github.com>
2022-06-02 13:03:57 -0400

    Added the ability to upload full Directory
    Updated to Django 4.0.5 and added the ability to upload a directory to MEDIA_ROOT.

--- photos/urls.py
+++ photos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('basic-upload/', views.BasicUploadView.as_view(), name='basic_upload'),
     path('progress-bar-upload/', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
     path('drag-and-drop-upload/', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
+    path('directory-upload-progress-bar/', views.DirectoryUploadView.as_view(), name='directory_upload_progress_bar'),
 ]
