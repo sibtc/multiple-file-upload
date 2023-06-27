a409043966c9d2d7919003d405a3028ca00e1f81
fmoreau <fabien.moreau@ifsttar.fr>
2023-06-27 17:53:47 +0200

    This commit makes the project compatible with the latest versions of python and django

--- mysite/photos/urls.py
+++ mysite/photos/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^clear/$', views.clear_database, name='clear_database'),
-    url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
-    url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
-    url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
+    re_path(r'^clear/$', views.clear_database, name='clear_database'),
+    re_path(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
+    re_path(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
+    re_path(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
 ]
