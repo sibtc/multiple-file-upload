4475120d9ea679d9ca96126b0c43f9385de522bc
fmoreau <fabien.moreau@ifsttar.fr>
2023-06-27 17:55:26 +0200

    This commit makes the project compatible with the latest versions of python and django

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
+from django.urls import re_path
 from django.conf import settings
-from django.conf.urls import re_path, include
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
